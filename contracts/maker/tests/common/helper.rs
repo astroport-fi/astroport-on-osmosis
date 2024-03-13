a85b7f7168bfbd896a1e95faca5261eecc2486be
Timofey <5527315+epanchee@users.noreply.github.com>
2024-03-13 15:43:35 +0400

    WIP: preapare test suite

--- contracts/maker/tests/common/helper.rs
+++ contracts/maker/tests/common/helper.rs
@@ -1,87 +1,32 @@
 #![allow(dead_code)]
 
-use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::Display;
 use std::str::FromStr;
 
 use anyhow::Result as AnyResult;
-use astroport::asset::{native_asset_info, Asset, AssetInfo, PairInfo};
+use astroport::asset::{Asset, AssetInfo, PairInfo};
 use astroport::factory::{PairConfig, PairType};
-use astroport::pair::Cw20HookMsg;
 use astroport::pair_concentrated::ConcentratedPoolParams;
-use astroport::token;
-use cosmwasm_schema::cw_serde;
+use astroport::{factory, pair};
 use cosmwasm_std::testing::MockApi;
 use cosmwasm_std::{
-    coin, coins, to_json_binary, Addr, Api, Coin, Decimal, Empty, GovMsg, IbcMsg, IbcQuery,
+    coins, to_json_binary, Addr, Api, Coin, Decimal, Empty, GovMsg, IbcMsg, IbcQuery,
     MemoryStorage, Storage,
 };
 use cw_multi_test::{
-    AddressGenerator, App, AppResponse, BankKeeper, BasicAppBuilder, Contract, ContractWrapper,
-    DistributionKeeper, Executor, FailingModule, StakeKeeper, WasmKeeper,
+    no_init, AddressGenerator, App, AppResponse, BankKeeper, BasicAppBuilder, Contract,
+    ContractWrapper, DistributionKeeper, Executor, FailingModule, StakeKeeper, WasmKeeper,
 };
 use cw_storage_plus::Item;
 use derivative::Derivative;
-use itertools::Itertools;
-use osmosis_std::types::osmosis::poolmanager::v1beta1::{
-    MsgSwapExactAmountOut, SwapAmountOutRoute,
-};
 
 use astroport_on_osmosis::pair_pcl::ExecuteMsg;
 use astroport_pcl_osmo::contract::{execute, instantiate, reply};
 use astroport_pcl_osmo::queries::query;
-use astroport_pcl_osmo::state::POOL_ID;
 use astroport_pcl_osmo::sudo::sudo;
 
 use crate::common::osmosis_ext::OsmosisStargate;
-
-const NATIVE_TOKEN_PRECISION: u8 = 6;
-
-const INIT_BALANCE: u128 = 1_000_000_000000;
-
-#[cw_serde]
-pub struct AmpGammaResponse {
-    pub amp: Decimal,
-    pub gamma: Decimal,
-    pub future_time: u64,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub enum TestCoin {
-    Cw20(String),
-    Cw20Precise(String, u8),
-    Native(String),
-}
-
-impl TestCoin {
-    pub fn denom(&self) -> Option<String> {
-        match self {
-            TestCoin::Native(denom) => Some(denom.clone()),
-            _ => None,
-        }
-    }
-
-    pub fn cw20_init_data(&self) -> Option<(String, u8)> {
-        match self {
-            TestCoin::Cw20(name) => Some((name.clone(), 6u8)),
-            TestCoin::Cw20Precise(name, precision) => Some((name.clone(), *precision)),
-            _ => None,
-        }
-    }
-
-    pub fn native(denom: &str) -> Self {
-        Self::Native(denom.to_string())
-    }
-
-    pub fn cw20(name: &str) -> Self {
-        Self::Cw20(name.to_string())
-    }
-
-    pub fn cw20precise(name: &str, precision: u8) -> Self {
-        Self::Cw20Precise(name.to_string(), precision)
-    }
-}
 
 pub type OsmoApp = App<
     BankKeeper,
@@ -95,23 +40,6 @@ pub type OsmoApp = App<
     FailingModule<GovMsg, Empty, Empty>,
     OsmosisStargate,
 >;
-
-pub fn init_native_coins(test_coins: &[TestCoin]) -> Vec<Coin> {
-    let mut test_coins: Vec<Coin> = test_coins
-        .iter()
-        .filter_map(|test_coin| match test_coin {
-            TestCoin::Native(name) => {
-                let init_balance = INIT_BALANCE * 10u128.pow(NATIVE_TOKEN_PRECISION as u32);
-                Some(coin(init_balance, name))
-            }
-            _ => None,
-        })
-        .collect();
-    test_coins.push(coin(INIT_BALANCE, "random-coin"));
-    test_coins.push(coin(INIT_BALANCE, "uosmo"));
-
-    test_coins
-}
 
 fn pair_contract() -> Box<dyn Contract<Empty>> {
     Box::new(
@@ -139,8 +67,35 @@ fn factory_contract() -> Box<dyn Contract<Empty>> {
     )
 }
 
+fn maker_contract() -> Box<dyn Contract<Empty>> {
+    Box::new(
+        ContractWrapper::new_with_empty(
+            astroport_maker_osmosis::execute::execute,
+            astroport_maker_osmosis::instantiate::instantiate,
+            astroport_maker_osmosis::query::query,
+        )
+        .with_reply_empty(astroport_maker_osmosis::reply::reply),
+    )
+}
+
 pub fn osmo_create_pair_fee() -> Vec<Coin> {
     coins(1000_000000, "uosmo")
+}
+
+fn common_pcl_params() -> ConcentratedPoolParams {
+    ConcentratedPoolParams {
+        amp: f64_to_dec(40f64),
+        gamma: f64_to_dec(0.000145),
+        mid_fee: f64_to_dec(0.0026),
+        out_fee: f64_to_dec(0.0045),
+        fee_gamma: f64_to_dec(0.00023),
+        repeg_profit_threshold: f64_to_dec(0.000002),
+        min_price_scale_delta: f64_to_dec(0.000146),
+        price_scale: Decimal::one(),
+        ma_half_time: 600,
+        track_asset_balances: None,
+        fee_share: None,
+    }
 }
 
 const FACTORY_ADDRESS: &str = include_str!("../../../pair_concentrated/src/factory_address");
@@ -171,60 +126,50 @@ impl<'a> AddressGenerator for HackyAddressGenerator<'a> {
     }
 }
 
+pub const ASTRO_DENOM: &str = "astro";
+
 #[derive(Derivative)]
 #[derivative(Debug)]
 pub struct Helper {
     #[derivative(Debug = "ignore")]
     pub app: OsmoApp,
     pub owner: Addr,
-    pub assets: HashMap<TestCoin, AssetInfo>,
+    pub coin_registry: Addr,
     pub factory: Addr,
-    pub pair_addr: Addr,
-    pub lp_token: String,
     pub maker: Addr,
 }
 
 impl Helper {
-    pub fn new(
-        owner: &Addr,
-        test_coins: Vec<TestCoin>,
-        params: ConcentratedPoolParams,
-    ) -> AnyResult<Self> {
+    pub fn new(owner: &Addr) -> AnyResult<Self> {
         let wasm_keeper =
             WasmKeeper::new().with_address_generator(HackyAddressGenerator::default());
         let mut app = BasicAppBuilder::new()
             .with_stargate(OsmosisStargate::default())
             .with_wasm(wasm_keeper)
-            .build(|router, _, storage| {
-                router
-                    .bank
-                    .init_balance(storage, owner, init_native_coins(&test_coins))
-                    .unwrap()
-            });
-
-        let asset_infos_vec = test_coins
-            .iter()
-            .cloned()
-            .map(|coin| {
-                let asset_info = match &coin {
-                    TestCoin::Native(denom) => native_asset_info(denom.clone()),
-                    TestCoin::Cw20(..) | TestCoin::Cw20Precise(..) => {
-                        unimplemented!("CW20 is not supported")
-                    }
-                };
-                (coin, asset_info)
-            })
-            .collect::<Vec<_>>();
+            .build(no_init);
 
         let pair_code_id = app.store_code(pair_contract());
         let factory_code_id = app.store_code(factory_contract());
-        let pair_type = PairType::Custom("concentrated".to_string());
 
-        let maker = Addr::unchecked("fake_maker");
+        let maker_code_id = app.store_code(maker_contract());
+        let maker = app.instantiate_contract(
+            maker_code_id,
+            owner.clone(),
+            &astroport_maker_osmosis::instantiate::InstantiateMsg {
+                owner: owner.to_string(),
+                astro_denom: ASTRO_DENOM.to_string(),
+                satellite: "satellite".to_string(),
+                max_spread: Decimal::percent(10),
+                collect_cooldown: None,
+            },
+            &[],
+            "Maker",
+            None,
+        )?;
 
         let coin_registry_id = app.store_code(coin_registry_contract());
 
-        let coin_registry_address = app
+        let coin_registry = app
             .instantiate_contract(
                 coin_registry_id,
                 owner.clone(),
@@ -237,27 +182,13 @@ impl Helper {
             )
             .unwrap();
 
-        app.execute_contract(
-            owner.clone(),
-            coin_registry_address.clone(),
-            &astroport::native_coin_registry::ExecuteMsg::Add {
-                native_coins: vec![
-                    ("uosmo".to_owned(), 6),
-                    ("uusd".to_owned(), 6),
-                    ("rc".to_owned(), 6),
-                    ("foo".to_owned(), 5),
-                ],
-            },
-            &[],
-        )
-        .unwrap();
         let init_msg = astroport::factory::InstantiateMsg {
             fee_address: Some(maker.to_string()),
             pair_configs: vec![PairConfig {
                 code_id: pair_code_id,
                 maker_fee_bps: 5000,
                 total_fee_bps: 0u16, // Concentrated pair does not use this field,
-                pair_type: pair_type.clone(),
+                pair_type: PairType::Custom("concentrated".to_string()),
                 is_disabled: false,
                 is_generator_disabled: false,
             }],
@@ -265,7 +196,7 @@ impl Helper {
             generator_address: None,
             owner: owner.to_string(),
             whitelist_code_id: 234u64,
-            coin_registry_address: coin_registry_address.to_string(),
+            coin_registry_address: coin_registry.to_string(),
         };
 
         // Set marker in storage that the next contract is factory. We need this to have exact FACTORY_ADDRESS constant
@@ -281,159 +212,93 @@ impl Helper {
             None,
         )?;
 
-        let asset_infos = asset_infos_vec
-            .clone()
-            .into_iter()
-            .map(|(_, asset_info)| asset_info)
-            .collect_vec();
-        let init_pair_msg = astroport::factory::ExecuteMsg::CreatePair {
-            pair_type,
-            asset_infos: asset_infos.clone(),
-            init_params: Some(to_json_binary(&params).unwrap()),
-        };
-
-        app.execute_contract(
-            owner.clone(),
-            factory.clone(),
-            &init_pair_msg,
-            &osmo_create_pair_fee(),
-        )?;
-
-        let resp: PairInfo = app.wrap().query_wasm_smart(
-            &factory,
-            &astroport::factory::QueryMsg::Pair { asset_infos },
-        )?;
-
         Ok(Self {
             app,
             owner: owner.clone(),
-            assets: asset_infos_vec.into_iter().collect(),
+            coin_registry,
             factory,
-            pair_addr: resp.contract_addr,
-            lp_token: resp.liquidity_token.to_string(),
-            maker: maker,
+            maker,
         })
     }
 
-    pub fn provide_liquidity(&mut self, sender: &Addr, assets: &[Asset]) -> AnyResult<AppResponse> {
-        self.provide_liquidity_with_slip_tolerance(
-            sender,
-            assets,
-            Some(f64_to_dec(0.5)), // 50% slip tolerance for testing purposes
-        )
+    pub fn create_pair(
+        &mut self,
+        asset_infos: &[AssetInfo],
+    ) -> AnyResult<PairInfo> {
+        let native_coins = asset_infos.iter()
+
+        self.app
+            .execute_contract(
+                self.owner.clone(),
+                self.coin_registry.clone(),
+                &astroport::native_coin_registry::ExecuteMsg::Add {
+                    native_coins,
+                },
+                &[],
+            )
+            .unwrap();
+
+        let asset_infos = asset_infos.to_vec();
+        self.app
+            .execute_contract(
+                Addr::unchecked("permissionless"),
+                self.factory.clone(),
+                &factory::ExecuteMsg::CreatePair {
+                    pair_type: PairType::Custom("concentrated".to_string()),
+                    asset_infos: asset_infos.clone(),
+                    init_params: Some(to_json_binary(&common_pcl_params()).unwrap()),
+                },
+                &osmo_create_pair_fee(),
+            )
+            .map(|_| self.query_pair_info(&asset_infos))
     }
 
-    pub fn provide_liquidity_with_slip_tolerance(
+    pub fn provide(
         &mut self,
+        pair: &Addr,
         sender: &Addr,
         assets: &[Asset],
-        slippage_tolerance: Option<Decimal>,
     ) -> AnyResult<AppResponse> {
-        let funds =
-            assets.mock_coins_sent(&mut self.app, sender, &self.pair_addr, SendType::Allowance);
+        let funds = assets
+            .iter()
+            .map(|x| x.as_coin().unwrap())
+            .collect::<Vec<_>>();
 
         let msg = ExecuteMsg::ProvideLiquidity {
             assets: assets.to_vec(),
-            slippage_tolerance,
+            slippage_tolerance: Some(f64_to_dec(0.5)),
             auto_stake: None,
             receiver: None,
         };
 
         self.app
-            .execute_contract(sender.clone(), self.pair_addr.clone(), &msg, &funds)
-    }
-
-    pub fn withdraw_liquidity(
-        &mut self,
-        sender: &Addr,
-        amount: u128,
-        assets: Vec<Asset>,
-    ) -> AnyResult<AppResponse> {
-        self.app.execute_contract(
-            sender.clone(),
-            self.pair_addr.clone(),
-            &ExecuteMsg::WithdrawLiquidity { assets },
-            &coins(amount, &self.lp_token),
-        )
+            .execute_contract(sender.clone(), pair.clone(), &msg, &funds)
     }
 
     pub fn swap(
         &mut self,
+        pair: &Addr,
         sender: &Addr,
         offer_asset: &Asset,
         max_spread: Option<Decimal>,
-    ) -> AnyResult<AppResponse> {
-        self.swap_full_params(sender, offer_asset, max_spread, None)
-    }
-
-    pub fn swap_full_params(
-        &mut self,
-        sender: &Addr,
-        offer_asset: &Asset,
-        max_spread: Option<Decimal>,
-        belief_price: Option<Decimal>,
     ) -> AnyResult<AppResponse> {
         match &offer_asset.info {
-            AssetInfo::Token { contract_addr } => {
-                let msg = token::ExecuteMsg::Send {
-                    contract: self.pair_addr.to_string(),
-                    amount: offer_asset.amount,
-                    msg: to_json_binary(&Cw20HookMsg::Swap {
-                        ask_asset_info: None,
-                        belief_price,
-                        max_spread,
-                        to: None,
-                    })
-                    .unwrap(),
-                };
-
-                self.app
-                    .execute_contract(sender.clone(), contract_addr.clone(), &msg, &[])
-            }
-            AssetInfo::NativeToken { .. } => {
-                let funds = offer_asset.mock_coin_sent(
-                    &mut self.app,
-                    sender,
-                    &self.pair_addr,
-                    SendType::None,
-                );
-
-                let msg = ExecuteMsg::Swap {
+            AssetInfo::NativeToken { .. } => self.app.execute_contract(
+                sender.clone(),
+                pair.clone(),
+                &pair::ExecuteMsg::Swap {
                     offer_asset: offer_asset.clone(),
                     ask_asset_info: None,
-                    belief_price,
+                    belief_price: None,
                     max_spread,
                     to: None,
-                };
-
-                self.app
-                    .execute_contract(sender.clone(), self.pair_addr.clone(), &msg, &funds)
-            }
+                },
+                &[offer_asset.as_coin().unwrap()],
+            ),
+            AssetInfo::Token { .. } => unimplemented!("cw20 not implemented"),
         }
     }
 
-    pub fn reverse_swap(
-        &mut self,
-        sender: &Addr,
-        ask_asset: &Asset,
-        max_offer_asset: &Asset,
-    ) -> AnyResult<AppResponse> {
-        let max_offer_coin = max_offer_asset.as_coin().unwrap();
-        let pool_id = POOL_ID
-            .query(&self.app.wrap(), self.pair_addr.clone())
-            .unwrap();
-        let msg = MsgSwapExactAmountOut {
-            sender: sender.to_string(),
-            token_out: Some(ask_asset.as_coin().unwrap().into()),
-            token_in_max_amount: max_offer_coin.amount.to_string(),
-            routes: vec![SwapAmountOutRoute {
-                pool_id,
-                token_in_denom: max_offer_coin.denom,
-            }],
-        };
-
-        self.app.execute(sender.clone(), msg.into())
-    }
     pub fn native_balance(&self, denom: &str, user: &Addr) -> u128 {
         self.app
             .wrap()
@@ -441,13 +306,6 @@ impl Helper {
             .unwrap()
             .amount
             .u128()
-    }
-
-    pub fn coin_balance(&self, coin: &TestCoin, user: &Addr) -> u128 {
-        match &self.assets[coin] {
-            AssetInfo::NativeToken { denom } => self.native_balance(denom, user),
-            AssetInfo::Token { .. } => unimplemented!("CW20 is not supported"),
-        }
     }
 
     pub fn give_me_money(&mut self, assets: &[Asset], recipient: &Addr) {
@@ -459,85 +317,6 @@ impl Helper {
                 .send_tokens(self.owner.clone(), recipient.clone(), &funds)
                 .unwrap();
         }
-    }
-}
-
-#[derive(Clone, Copy)]
-pub enum SendType {
-    Allowance,
-    Transfer,
-    None,
-}
-
-pub trait AssetExt {
-    fn mock_coin_sent(
-        &self,
-        app: &mut OsmoApp,
-        user: &Addr,
-        spender: &Addr,
-        typ: SendType,
-    ) -> Vec<Coin>;
-}
-
-impl AssetExt for Asset {
-    fn mock_coin_sent(
-        &self,
-        app: &mut OsmoApp,
-        user: &Addr,
-        spender: &Addr,
-        typ: SendType,
-    ) -> Vec<Coin> {
-        let mut funds = vec![];
-        match &self.info {
-            AssetInfo::Token { contract_addr } if !self.amount.is_zero() => {
-                let msg = match typ {
-                    SendType::Allowance => token::ExecuteMsg::IncreaseAllowance {
-                        spender: spender.to_string(),
-                        amount: self.amount,
-                        expires: None,
-                    },
-                    SendType::Transfer => token::ExecuteMsg::Transfer {
-                        recipient: spender.to_string(),
-                        amount: self.amount,
-                    },
-                    _ => unimplemented!(),
-                };
-                app.execute_contract(user.clone(), contract_addr.clone(), &msg, &[])
-                    .unwrap();
-            }
-            AssetInfo::NativeToken { denom } if !self.amount.is_zero() => {
-                funds = vec![coin(self.amount.u128(), denom)];
-            }
-            _ => {}
-        }
-
-        funds
-    }
-}
-
-pub trait AssetsExt {
-    fn mock_coins_sent(
-        &self,
-        app: &mut OsmoApp,
-        user: &Addr,
-        spender: &Addr,
-        typ: SendType,
-    ) -> Vec<Coin>;
-}
-
-impl AssetsExt for &[Asset] {
-    fn mock_coins_sent(
-        &self,
-        app: &mut OsmoApp,
-        user: &Addr,
-        spender: &Addr,
-        typ: SendType,
-    ) -> Vec<Coin> {
-        let mut funds = vec![];
-        for asset in self.iter() {
-            funds.extend(asset.mock_coin_sent(app, user, spender, typ));
-        }
-        funds
     }
 }
 
